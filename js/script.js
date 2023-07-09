3cb178fd8e8aaacd8451e9767b99d3a07e22d9c8
MAbdurahman <mdbdrrhm@gmail.com>
2023-07-09 12:21:30 -0500

    Add necessary directories and files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*-----Javascript for Project Name */
+$(document).ready(function() {
+   // Fakes the loading setting a timeout
+   setTimeout(function() {
+      $('body').addClass('loaded');
+   }, 1500);
+});
+
+
